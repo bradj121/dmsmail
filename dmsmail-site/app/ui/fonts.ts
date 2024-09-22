b14f668d98df790fa2dea5f75355703c729f1929
Brad Johnson <bradj121@gmail.com>
2024-09-22 16:34:23 -0400

    baby-steps

---
+++ dmsmail-site/app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import { VT323 } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ["400", "700"] });
+export const vt323 = VT323({ subsets: ['latin'], weight: ["400"]});
